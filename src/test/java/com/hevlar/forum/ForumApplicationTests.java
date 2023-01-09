aa34eeac196458d6322f02c74d11fee45e91adaa
Dennis Cai <dennis.cai@zuhlke.com>
2023-01-09 23:17:00 +0800

    add h2 for testing

--- src/test/java/com/hevlar/forum/ForumApplicationTests.java
+++ src/test/java/com/hevlar/forum/ForumApplicationTests.java
@@ -2,11 +2,16 @@ package com.hevlar.forum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ForumApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     void contextLoads() {
