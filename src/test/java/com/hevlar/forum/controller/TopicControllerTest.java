2ea6ab46c0d9058b16c526aac7195ae03fa336b8
Dennis Cai <dennis.cai@zuhlke.com>
2023-01-21 10:02:31 +0800

    add test

--- src/test/java/com/hevlar/forum/controller/TopicControllerTest.java
+++ src/test/java/com/hevlar/forum/controller/TopicControllerTest.java
@@ -11,14 +11,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(TopicController.class)
@@ -34,6 +36,39 @@ public class TopicControllerTest {
     ObjectMapper objectMapper;
 
     final String api = "/api/v1/topics";
+
+    @Test
+    public void givenTopicsExists_whenListTopic_thenReturnTopicList() throws Exception {
+        List<Topic> topicList = List.of(
+                new Topic("first topic"),
+                new Topic("second topic")
+        );
+        given(topicService.list()).willReturn(topicList);
+
+        mockMvc.perform(get(api))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    public void whenCreateTopic_thenReturn301CreatedAndTopic() throws Exception {
+        Topic topic = Topic.builder()
+                .topicId(1L)
+                .title("topic")
+                .build();
+        given(topicService.create(any())).willReturn(topic);
+
+        RequestBuilder request = post(api)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(topic));
+        mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpectAll(
+                        jsonPath("$.topicId").value("1"),
+                        jsonPath("$.title").value("topic")
+                );
+    }
 
     @Test
     public void givenTopicDoesNotExists_whenGetTopic_thenReturn404NotFound() throws Exception {
