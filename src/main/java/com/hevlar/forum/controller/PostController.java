e08c19205401e10b36a6e67a3f928e28e1bdde33
Dennis Cai <dennis.cai@zuhlke.com>
2023-02-07 18:11:19 +0800

    fix spring security csrf

--- src/main/java/com/hevlar/forum/controller/PostController.java
+++ src/main/java/com/hevlar/forum/controller/PostController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = {"*"})
 @RequestMapping("/api/v1")
 public class PostController {
 
--- src/main/java/com/hevlar/forum/security/ForumSecurityConfiguration.java
+++ src/main/java/com/hevlar/forum/security/ForumSecurityConfiguration.java
@@ -5,10 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
 public class ForumSecurityConfiguration {
+
+//    @Bean
+//    public DefaultSecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
+//        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+//        // set the name of the attribute the CsrfToken will be populated on
+//        requestHandler.setCsrfRequestAttributeName(null);
+//        http
+//                // ...
+//                .csrf((csrf) -> csrf
+//                        .csrfTokenRequestHandler(requestHandler)
+//                );
+//        return http.build();
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
@@ -16,6 +31,11 @@ public class ForumSecurityConfiguration {
                 .anyRequest().permitAll()
                 .and()
                 .cors()
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+//                .sessionAuthenticationStrategy(new NullAuthenticatedSessionStrategy())
                 .and()
                 .formLogin().disable();
         return httpSecurity.build();
